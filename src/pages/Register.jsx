0ced83eb8dcb5542a883cc4fd4c24104a11aac6b
user <ujjalroy7862@gmail.com>
2022-12-04 14:17:48 +0600

    now controll the login,register part using setupUser function

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -17,8 +17,15 @@ const Register = () => {
 
   const [values, setValues] = useState(initialState);
 
-  const { user, isLoading, showAlert, displayAlert, registerUser, loginUser } =
-    useAppContext();
+  const {
+    user,
+    isLoading,
+    showAlert,
+    displayAlert,
+    registerUser,
+    loginUser,
+    setupUser,
+  } = useAppContext();
 
   const toggleMember = () => {
     setValues({ ...values, isMember: !values.isMember });
@@ -39,9 +46,17 @@ const Register = () => {
     const currentUser = { name, email, password };
 
     if (isMember) {
-      loginUser(currentUser);
+      setupUser({
+        currentUser,
+        endPoint: "login",
+        alertText: "Login Successful!! Redirecting...",
+      });
     } else {
-      registerUser(currentUser);
+      setupUser({
+        currentUser,
+        endPoint: "register",
+        alertText: "User Created!! Redirecting...",
+      });
     }
 
     console.log(values);
